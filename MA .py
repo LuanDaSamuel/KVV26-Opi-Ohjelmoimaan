9a7bd33cea8197f05e1721533b8670f48d1e6e24
SinevaDeCoder <minhluan.sam@gmail.com>
2026-04-14 09:14:37 +0300

    Add various calculations and user prompts

--- MA .py
+++ MA .py
@@ -1,1 +1,94 @@
+ 
+Chats
+Search Messenger
+
+Samoi
+Loading...
+
+Mon 1:38 PM
+You sent
+print("I will calculate your input, please give me two numbers")
+
+a = input("How many people are there around you ? ")
+number_1= int(a)
+b = input("How old are you ? ")
+number_2= int(b)
+c = number_1 + number_2
+print("THis is your final value", c)
+Mon 2:10 PM
+You sent
+a=20
+b=4
+
+
+print("given numbers are:", a, "and", b)
+print("total is", 20 + 4)
+print("difference is", 20 - 4)
+print("quotient is", 20 * 4)
+print("fraction is", 20 / 4)
+You sent
+a= int(input("Give me your height value: "))
+b= int(input("Give me your base side value: "))
+
+triangle= 1/2 * a * b
+
+print("This is your triangle area value", triangle)
+Mon 6:36 PM
+You sent
+#Tehtävä 1
+
+name = input("Please enter your name: ")
+print("Nice to meet you", name, "have a nice day")
+
+#Tehtävä 2
+
+print("I will calculate your input, please give me two numbers")
+
+a = input("How many people are there around you ? ")
+number_1= int(a)
+b = input("How old are you ? ")
+number_2= int(b)
+c = number_1 + number_2
+print("This is your final value", c)
+print("Thank you for using my service.")
+
+#Tehtävä 3
+
+a=20
+b=4
+
+
+print("given numbers are:", a, "and", b)
+print("total is", 20 + 4)
+print("difference is", 20 - 4)
+print("quotient is", 20 * 4)
+print("fraction is", 20 / 4)
+
+#Tehtävä 4
+
+a= int(input("Give me your height value: "))
+b= int(input("Give me your base side value: "))
+
+triangle= 1/2 * a * b
+
+print("This is your triangle area value", triangle)
+
+#Tehtävä 5
+
+
+
+
+#Tehtävä 6
+v_ltr = 1.65 
+u_ltr = 2
+
+print("Gasoline price per liter old price: ", v_ltr)
+print("Gasoline price per liter current price: ", u_ltr)
+
+print("The price of gasoline has increased by: ", (u_ltr - v_ltr) / v_ltr * 100, "%")
+
+print("The price of gasoline has increased by: ", round((u_ltr - v_ltr) / v_ltr * 100, 2), "%")
+
+
+          
 
