40ab43099af57a748a329932e23b468adc6107d7
SinevaDeCoder <minhluan.sam@gmail.com>
2026-04-14 09:49:12 +0300

    Correct input type for division calculation
    Fixed input handling by converting user input to float before division.

--- 13.04.2026 muistipanot .py
+++ 13.04.2026 muistipanot .py
@@ -1,1 +1,35 @@
+# lasketaan kahden luvun osamäärä
 
+x = input("Anna jaettava luku ")
+
+y = input("Anna jakaja ")
+
+#osamäärä
+
+z = x / y
+
+#näytetään tulos
+
+print("lukujen", x, y, "osamäärä on", z)
+
+# lasketaan kahden luvun osamäärä
+
+#muutetaan käyttäjän syöte oikeaan tietotyyppin (int, float)
+
+x = input("Anna jaettava luku ")
+
+x = float(x)
+
+y = input("Anna jakaja ")
+
+y = float(y)
+
+#osamäärä
+
+z = x / y
+
+#näytetään tulos
+
+print("lukujen", x, y, "osamäärä on", z)
+
+#HUOM: muista desimaalierotin on piste.
