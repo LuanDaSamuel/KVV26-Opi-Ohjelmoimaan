aa3ce9af9367a6b057fbd047eb7ac8a97b4ee442
SinevaDeCoder <minhluan.sam@gmail.com>
2026-04-14 09:47:46 +0300

    Implement string manipulation and print examples
    Added string manipulation examples and f-string usage.

--- TI muistipanot .py
+++ TI muistipanot .py
@@ -1,1 +1,63 @@
+#String
+nimi = "Samuel Sierra"
+print(nimi)
+
+
+print(nimi[0])
+print(nimi[1])
+print(nimi[2])
+print(nimi[3])
+print(nimi[4])
+print(nimi[5])
+print(nimi[6])
+print(nimi[7])  
+print(nimi[8])
+print(nimi[9])
+print(nimi[10])
+print(nimi[11])
+print(nimi[12])
+
+
+etunimi= nimi[0:6]
+sukunimi= nimi[7:13]
+
+print("Etunimi:", etunimi)
+print("Sukunimi:", sukunimi)
+
+nimi.lower()
+print(nimi.lower())
+nimi.upper()
+print(nimi.upper())
+
+##Practicing
+a = "Python"
+
+len(a)
+a[0]
+a[1]
+a[2]
+a[3]
+a[4]
+a[5]
+
+print(len(a))
+
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
+
+a.lower()
+a.upper()
+
+print(a.lower())
+print(a.upper())
+
+reversed_a = a[::-1]
+print(reversed_a)
+
+
+#f-string
 
