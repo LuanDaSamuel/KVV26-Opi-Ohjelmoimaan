c2ed4209f3c57b02bbf67ea100390509766839af
SinevaDeCoder <minhluan.sam@gmail.com>
2026-04-15 10:14:04 +0300

    Add tasks for name input and decimal division
    Added new tasks for decimal division and input handling.

--- Ti tehtävä .py
+++ Ti tehtävä .py
@@ -2,3 +2,13 @@
 a = input("Please enter your name: ")
 len(a)
 print(len(a))
+
+#Tehtävä 2
+
+a = float(input("Enter the first decimal number: "))
+b = float(input("Enter the second decimal number: "))
+c = a / b
+print("The result of dividing", a, "by", b, "is:", c)
+
+#Tehtävä 3
+
