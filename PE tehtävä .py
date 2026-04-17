df96b31601385fc37c7391287cb9a92fb3d41369
SinevaDeCoder <minhluan.sam@gmail.com>
2026-04-17 10:46:27 +0300

    Add second task for a casino game simulation

--- PE tehtävä .py
+++ PE tehtävä .py
@@ -1,3 +1,4 @@
+#Tehtävä 1
 import random
 
 a = int(random.randint(1, 100))
@@ -23,3 +24,29 @@ else:
             biggest = a
             smallest = c
             print(biggest, smallest)
+
+#Tehtävä 2
+import random
+
+money=10
+lost = 1
+kasino = random.randint(1,36)
+
+for i in range(10):
+    minun = int(input("Enter a number between 1 and 36: "))
+    print(f"You got number {minun}")
+    #vertaillaan kumpi suurempi
+    if minun > kasino:
+        print(f"You won ! {minun} is bigger than {kasino}")
+        money = money - lost + 36
+        print(f"You have {money} money left.") 
+    elif minun == kasino:
+        print(f"Draw.")
+        money =  money + lost - lost
+        print(f"You have {money} money left.")
+    else:
+        print(f"Kasino voitti!")
+        money -= lost
+        print(f"You have {money} money left.")
+    continue
+print(f"Game over. You have {money} money left.")
